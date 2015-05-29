7fa8a807e394f5c39c53bd7d1c2b8932cfd2ddb1
David-Alexandre Chanel <david.alexandre.chanel.ext@gmail.com>
2015-05-29 19:08:11 +0200

    while-loop.sh  example script

---
+++ while-loop.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# This script is an example of a while loop saying "you fuck my wife" if triggered on mac (particularly funny on a french mac)
+
+# Uncomment next line to enable debug output
+#set -x
+
+[ "$1" = "-h" ] || [ "$1" = "--help" ] || [ $# != 0 ] && { echo "Usage : $0"; exit 1; }
+
+#!/bin/bash
+x=1
+while [ $x -le 5 ]
+do
+  say "You fuck my wife"
+  x=$(( $x + 1 ))
+done
+
